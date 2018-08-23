d12d7e25e1350bd3eb9c198443ad0cc0cc022706
Naoya Iijima <naoyaiijima@hiroshima-u.ac.jp>
2018-08-23 23:51:05 +0900

    create file(addImages.py) and add images

---
+++ BasicProcessing/addImages.py
@@ -0,0 +1,13 @@
+import cv2
+
+img1 = cv2.imread("./face.jpeg")
+img2 = cv2.imread("./road.jpeg")
+
+for i in range(0,10,1):
+    res = cv2.addWeighted(img1, 1-i*0.1, img2, i*0.1, 0)
+    cv2.imshow("image", res)
+    key = cv2.waitKey(0)
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
